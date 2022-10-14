ed79bd2dae4b5b6a01aeb86c3a24f18be2df3296
DaryaKhod <khodakova0305200157@gmail.com>
2022-10-15 00:49:43 +0300

    First commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int j = 0;
+    int jz = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button1 = findViewById(R.id.button1);
+        final Button button2 = findViewById(R.id.button2);
+        final TextView text = findViewById(R.id.text1);
+        final TextView text2 = findViewById(R.id.text2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            j++;
+            button1.setText("НАЖАТА");
+            text.setText(Integer.toString(j));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jz++;
+                button2.setText("PRESSED");
+                text2.setText(Integer.toString(jz));
+            }
+        });
+
+    }
+}
